be7695b1fc7dadd52f3b4f4e58ca9d7f745ac991
Kurtis Pivert <kpivert@asn-online.org>
2022-11-15 16:50:54 -0500

    Drafted Basic Report

---
+++ 00_scratch.R
@@ -0,0 +1,116 @@
+highchart() |>
+  hc_title(
+    text = title,
+    style = list(fontSize = "15px")
+  ) |> 
+  hc_chart(
+    type = type,
+    polar = is_polar,
+    inverted = invt
+  ) |> 
+  hc_xAxis(
+    categories = df$name
+  ) |> 
+  hc_yAxis(
+    visible = yvisible
+  ) |> 
+  hc_tooltip(
+    outside = TRUE
+  ) |> 
+  hc_add_series(
+    df,
+    name = "Fruit Consumption",
+    showInLegend = FALSE
+  )
+
+
+highchart() |> 
+  hc_chart(
+    type = "steamgraph"
+  )
+
+
+
+hchart(
+  df_long |> 
+    filter(
+      Variable %in% c(
+        "US MD Matched", "DO Matched", "US-IMG Matched", "IMG Matched", 
+        "Canadian Matched", "Fifth Pathway Matched Matched"
+      )
+    ),
+  "streamgraph", 
+  hcaes(
+    x = Year, 
+    y = Value, 
+    group = Variable
+  )
+) |>
+  hc_tooltip(
+    sort = TRUE, 
+    table = TRUE 
+    ) |> 
+  hc_yAxis(
+    # options = list(visible = "FALSE")
+    visible = FALSE
+  ) |> 
+  hc_title(
+    text = "ASN Nephrology Fellow Survey 2014–2022"
+  ) |>
+  hc_tooltip(
+    sort = TRUE, 
+    table = TRUE, 
+    valueSuffix = list("%")
+  ) 
+
+
+test_hc <- 
+  highchart() |> 
+  hc_add_series(
+    data = df_long |> 
+      filter(
+        Variable %in% c(
+          "US MD Matched", "DO Matched", "US-IMG Matched", "IMG Matched", 
+          "Canadian Matched", "Fifth Pathway Matched Matched"
+        )
+      ),
+    type = "streamgraph",
+    hcaes(
+      x = Year, 
+      y = Value, 
+      group = Variable
+    )
+  ) |> 
+  hc_tooltip(
+    sort = TRUE, 
+    table = TRUE 
+  ) |> 
+  hc_yAxis(
+    visible = FALSE
+  )
+
+
+asn_hc_theme <- hc_theme_merge(
+  hc_theme_smpl(),
+  hc_theme(
+    colors  = c("#00468b","#319b42", "#ff8200", "#0077C8", "#FFB500")
+  )
+)
+
+test_hc |>
+  hc_add_theme(asn_hc_theme)
+
+
+
+
+
+
+
+
+df_long |> 
+  filter(
+    Variable %in% c(
+      "US MD Matched", "DO Matched", "US-IMG Matched", "IMG Matched", 
+      "Canadian Matched", "Fifth Pathway Matched Matched"
+      )
+    )
